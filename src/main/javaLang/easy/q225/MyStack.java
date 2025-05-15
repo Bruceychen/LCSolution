19013177bcc4ee5b35ef3c3772fee746668b9ac5
Bruce Chen <r08006@gmail.com>
2025-05-15 08:47:18 +0800

    update 225 solution

---
+++ src/main/javaLang/easy/q225/MyStack.java
@@ -0,0 +1,33 @@
+package main.javaLang.easy.q225;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> dataq;
+
+    public MyStack() {
+        dataq = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        dataq.add(Integer.valueOf(x));
+        if (dataq.size() > 1) {
+            for (int i = 1; i < dataq.size(); i++) {
+                dataq.add(dataq.remove());
+            }
+        }
+    }
+
+    public int pop() {
+        return dataq.remove().intValue();
+    }
+
+    public int top() {
+        return dataq.peek().intValue();
+    }
+
+    public boolean empty() {
+        return dataq.isEmpty();
+    }
+}
